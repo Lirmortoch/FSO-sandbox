615ee18694fca250e227b650f83bee30e43fdaf2
Lirmortoch <dima2111323@gmail.com>
2025-11-09 20:10:48 +0300

    add mongodb functionality to backend

--- partThree/renderingCollectionBackEnd/index.js
+++ partThree/renderingCollectionBackEnd/index.js
@@ -1,33 +1,16 @@
+const Note = require('./models/note');
 const express = require('express');
-//const cors = require('cors');
 const app = express();
 const {loadEnvFile} = require('node:process');
 loadEnvFile();
 
 app.use(express.json());
-//app.use(cors());
 app.use(express.static('dist'));
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
-
 app.get('/api/notes', (request, response) => {
-  response.json(notes);
+  Note.find({}).then(notes => {
+    response.json(notes);
+  });
 });
 app.get('/api/notes/:id', (request, response) => {
   const id = request.params.id
