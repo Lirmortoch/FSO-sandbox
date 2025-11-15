376fddc8f07bf82d8ec2bc08405ca8d0db28b809
Lirmortoch <dima2111323@gmail.com>
2025-11-15 17:33:49 +0300

    Add validation for note model

--- partThree/renderingCollectionBackEnd/index.js
+++ partThree/renderingCollectionBackEnd/index.js
@@ -16,6 +16,9 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' });
   }
+  else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error);
 }
@@ -47,7 +50,7 @@ app.delete('/api/notes/:id', (request, response) => {
     .catch(error => next(error));
 });
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body;
 
   if (!body.content) {
@@ -61,9 +64,11 @@ app.post('/api/notes', (request, response) => {
     important: body.important || false,
   });
 
-  note.save().then(savedNote => {
+  note.save()
+    .then(savedNote => {
     response.json(savedNote);
-  });
+    })
+    .catch(error => next(error));
 });
 
 app.put('/api/notes/:id', (request, response, next) => {
