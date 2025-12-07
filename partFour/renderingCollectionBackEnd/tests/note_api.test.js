796d5fc5cbb4a9c8483afcc2a7ffdaa11d8e630c
Lirmortoch <dima2111323@gmail.com>
2025-12-07 18:12:39 +0300

    add new test and statement for errorHandler

--- partFour/renderingCollectionBackEnd/tests/note_api.test.js
+++ partFour/renderingCollectionBackEnd/tests/note_api.test.js
@@ -115,7 +115,7 @@ describe('when there is initially one user in db', () => {
     await User.deleteMany({});
 
     const passwordHash = await bcrypt.hash('sekret', 10);
-    const user = new User({username: 'root', passwordHash });
+    const user = new User({ username: 'root', passwordHash });
 
     await user.save();
   })
@@ -141,7 +141,30 @@ describe('when there is initially one user in db', () => {
     const usernames = usersAtEnd.map(u => u.username);
     assert(usernames.includes(newUser.username));
   })
+
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    assert(result.body.error.includes('expected `username` to be unique'))
+
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+  })
 });
+
+
 
 after(async () => {
   await mongoose.connection.close()
