383138b0b0e54b4aed7787776e1a0b6e078d0da0
Lirmortoch <dima2111323@gmail.com>
2025-12-22 17:38:27 +0300

    add login form and axios service

--- partFive/renderingCollectionFrontEnd/src/App.jsx
+++ partFive/renderingCollectionFrontEnd/src/App.jsx
@@ -10,6 +10,8 @@ const App = () => {
   const [newNote, setNewNote] = useState('');
   const [showAll, setShowAll] = useState(true);
   const [errorMessage, setErrorMessage] = useState('some error happened...');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
   const toggleImportanceOf = (id) => {
     const note = notes.find(n => n.id === id);
@@ -41,6 +43,11 @@ const App = () => {
 
   useEffect(hook, []);
 
+  const handleLogin = (event) => {
+    event.preventDefault();
+    console.log('logging in with', username, password);
+  }
+
   function addNote(event) {
     event.preventDefault();
     const noteObject = {
@@ -71,6 +78,24 @@ const App = () => {
       <main className='rendering-collection'>
         <h1 className='rendering-collection__title'>Notes</h1>
         <Notification message={errorMessage} />
+
+        <h2>Login</h2>
+        <form onSubmit={handleLogin}>
+          <fieldset>
+            <label>
+              username
+              <input type='text' value={username} onChange={({ target }) => setUsername(target.value)}></input>
+            </label>
+          </fieldset>
+          <fieldset>
+            <label>
+              password
+              <input type='password' value={username} onChange={({ target }) => setPassword(target.value)}></input>
+            </label>
+          </fieldset>
+          <button type='submit'>Login</button>
+        </form>
+
         <button onClick={() => setShowAll(!showAll)} className='rendering-collection__important-btn'>
           show {showAll ? 'important' : 'all'}
         </button>
