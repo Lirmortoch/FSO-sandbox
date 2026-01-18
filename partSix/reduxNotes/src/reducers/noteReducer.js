18211f896c51329236c248f0c947c2e552716df4
Lirmortoch <dima2111323@gmail.com>
2026-01-18 14:57:31 +0300

    update app (seperate components)

--- partSix/reduxNotes/src/reducers/noteReducer.js
+++ partSix/reduxNotes/src/reducers/noteReducer.js
@@ -11,6 +11,8 @@ const initialState = [
   },
 ]
 
+const generateId = () => Number((Math.random() * 1000000).toFixed(0))
+
 const noteReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'NEW_NOTE':
