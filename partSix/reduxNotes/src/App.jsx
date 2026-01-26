346eb111fef0f1a68c1e53fcb07c8b8babb3ea94
Lirmortoch <dima2111323@gmail.com>
2026-01-26 15:25:09 +0300

    used Redux Thunk for improving code

--- partSix/reduxNotes/src/App.jsx
+++ partSix/reduxNotes/src/App.jsx
@@ -4,14 +4,13 @@ import { useDispatch } from 'react-redux'
 import NoteForm from './components/NoteForm.jsx'
 import Notes from './components/Notes'
 import VisibilityFilter from './components/VisibilityFilter.jsx'
-import { setNotes } from './reducers/noteReducer.js'
-import noteService from './services/notes.js'
+import { initializeNotes } from './reducers/noteReducer.js'
 
 const App = () => {
   const dispatch = useDispatch()
 
   useEffect(() => {
-    noteService.getAll().then(notes => dispatch(setNotes(notes)))
+    dispatch(initializeNotes())
   }, [dispatch])
 
   return (
--- partSix/reduxNotes/src/components/NoteForm.jsx
+++ partSix/reduxNotes/src/components/NoteForm.jsx
@@ -1,6 +1,5 @@
 import { useDispatch } from 'react-redux'
-import { createNote } from '../reducers/noteReducer'
-import noteService from '../services/notes'
+import { appendNote } from '../reducers/noteReducer'
 
 const NoteForm = () => {
   const dispatch = useDispatch()
@@ -10,8 +9,7 @@ const NoteForm = () => {
     const content = event.target.note.value
     event.target.note.value = ''
 
-    const newNote = await noteService.createNew(content)
-    dispatch(createNote(newNote))
+    dispatch(appendNote(content))
   }
 
   return (
