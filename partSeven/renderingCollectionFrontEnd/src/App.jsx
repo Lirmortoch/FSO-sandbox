5c1a8f9f2992491c30a3f9d8a60216542bf7fc25
Lirmortoch <dima2111323@gmail.com>
2026-02-04 14:34:59 +0300

    implement simple routing

--- partSeven/renderingCollectionFrontEnd/src/App.jsx
+++ partSeven/renderingCollectionFrontEnd/src/App.jsx
@@ -1,92 +1,56 @@
-import { useState, useEffect } from 'react';
+import { useState }  from 'react'
 
-import Note from './components/Note';
-import noteService from './services/notes';
-import Notification from './components/Notificatoin';
-import Footer from './components/Footer';
+const Home = () => (
+  <div> <h2>TKTL notes app</h2> </div>
+)
+
+const Notes = () => (
+  <div> <h2>Notes</h2> </div>
+)
+
+const Users = () => (
+  <div> <h2>Users</h2> </div>
+)
 
 const App = () => {
-  const [notes, setNotes] = useState([]);
-  const [newNote, setNewNote] = useState('');
-  const [showAll, setShowAll] = useState(true);
-  const [errorMessage, setErrorMessage] = useState('some error happened...');
+  const [page, setPage] = useState('home')
 
-  const toggleImportanceOf = (id) => {
-    const note = notes.find(n => n.id === id);
-    const changedNote = { ...note, important: !note.important };
-
-    noteService
-      .update(id, changedNote)
-      .then(returnedNote => {
-        setNotes(notes.map(note => note.id === id ? returnedNote : note))
-      })
-      .catch(error => {
-        setErrorMessage(
-          `Note '${note.content}' was already removed from server`
-        );
-        setTimeout(() => {
-          setErrorMessage(null);
-        }, 5000);
-        setNotes(notes.filter(n => n.id !== id));
-      });
+  const toPage = (page) => (event) => {
+    event.preventDefault()
+    setPage(page)
   }
 
-  const hook = () => {
-    noteService
-      .getAll()
-      .then(initialNotes => {
-        setNotes(initialNotes);
-      })
-  }
-
-  useEffect(hook, []);
-
-  function addNote(event) {
-    event.preventDefault();
-    const noteObject = {
-      content: newNote,
-      id: notes.length + 1,
-      important: false,
+  const content = () => {
+    if (page === 'home') {
+      return <Home />
+    } else if (page === 'notes') {
+      return <Notes />
+    } else if (page === 'users') {
+      return <Users />
     }
-
-    noteService
-      .create(noteObject)
-      .then(returnedNote => {
-        setNotes(notes.concat(returnedNote))
-        setNewNote('')
-      });
   }
-  function handleNoteChange(event) {
-    setNewNote(event.target.value);
-  }
-  
-  const notesToShow = showAll 
-  ? notes
-  : notes.filter(note => note.important);
 
-  console.log(notesToShow);
+  const padding = {
+    padding: 5
+  }
 
   return (
     <div>
-      <main className='rendering-collection'>
-        <h1 className='rendering-collection__title'>Notes</h1>
-        <Notification message={errorMessage} />
-        <button onClick={() => setShowAll(!showAll)} className='rendering-collection__important-btn'>
-          show {showAll ? 'important' : 'all'}
-        </button>
-        <ul className='rendering-collection__list rc-list'>
-          {notesToShow.map(note =>
-            <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)} />
-          )}
-        </ul>
-        <form onSubmit={addNote} className='rendering-collection__form rc-form'>
-          <input type='text' className='rc-form__input' value={newNote} onChange={handleNoteChange}/>
-          <button type='submit' onClick={addNote} className='rc-form__button'>Save</button>
-        </form>
-      </main>
-      <Footer />
+      <div>
+        <a href="" onClick={toPage('home')} style={padding}>
+          home
+        </a>
+        <a href="" onClick={toPage('notes')} style={padding}>
+          notes
+        </a>
+        <a href="" onClick={toPage('users')} style={padding}>
+          users
+        </a>
+      </div>
+
+      {content()}
     </div>
   )
 }
 
-export default App;
+export default App
