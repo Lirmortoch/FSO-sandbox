f32bc73651caf0f796220ad66302e336e29c38ff
Lirmortoch <dima2111323@gmail.com>
2026-02-11 13:17:26 +0300

    add styles for navbar

--- partSeven/renderingCollectionFrontEnd/src/App.jsx
+++ partSeven/renderingCollectionFrontEnd/src/App.jsx
@@ -9,7 +9,7 @@ import {
   useMatch,
 } from "react-router-dom"
 
-import { Table, Form, Button, Alert } from 'react-bootstrap'
+import { Table, Form, Button, Alert, Navbar, Nav } from 'react-bootstrap'
 
 
 const Home = () => (
@@ -140,15 +140,29 @@ const App = () => {
           {message}
         </Alert>
       )}
-      <div>
-        <Link style={padding} to="/">home</Link>
-        <Link style={padding} to="/notes">notes</Link>
-        <Link style={padding} to="/users">users</Link>
-        {user
-          ? <em>{user} logged in</em>
-          : <Link style={padding} to="/login">login</Link>
-        }
-      </div>
+
+      <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav'>
+          <Nav className='me-auto'>
+            <Nav.Link href='#' as='span'>
+              <Link style={padding} to="/">home</Link>
+            </Nav.Link>
+            <Nav.Link href='#' as='span'>
+              <Link style={padding} to="/notes">notes</Link>
+            </Nav.Link>
+            <Nav.Link href='#' as='span'>
+              <Link style={padding} to="/users">users</Link>
+            </Nav.Link>
+            <Nav.Link href='#' as='span'>
+              {user
+                ? <em>{user} logged in</em>
+                : <Link style={padding} to="/login">login</Link>
+              }
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
 
       <Routes>
         <Route path="/notes/:id" element={<Note note={note} />} />  
@@ -157,6 +171,7 @@ const App = () => {
         <Route path="/login" element={<Login onLogin={login} />} />
         <Route path="/" element={<Home />} />      
       </Routes> 
+
       <footer>
         <br />
         <em>Note app, Department of Computer Science 2024</em>
